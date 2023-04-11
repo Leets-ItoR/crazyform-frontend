13cddb88494adce44b9ec282f0da77527c1b2d33
Ubuntu <ubuntu@ip-172-31-44-168.ap-northeast-2.compute.internal>
2023-04-11 02:44:20 +0000

    Project init

---
+++ src/App.svelte
@@ -0,0 +1,60 @@
+<script>
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+    <a href="https://tailwindcss.com" target="_blank" rel="noreferrer"
+      ><img
+        src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.79614a5f61617ba49a0891494521226b.svg"
+        class="logo tailwindcss"
+        alt="tailwindcss Logo"
+        style="height:80px"
+      /></a
+    >
+  </div>
+  <h1>Vite + Svelte + tailwindcss</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .logo.tailwindcss:hover {
+    filter: drop-shadow(0 0 2em #38bdf9);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
