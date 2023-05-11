51c5c360c5361792aa0a894966b85cafb19c8cf5
hayamaster <dnfxmfkdbzos@naver.com>
2023-05-11 09:11:49 +0000

    modified my form

--- src/pages/myForm.svelte
+++ src/pages/myForm.svelte
@@ -4,23 +4,23 @@
 </script>
 
 <Sidebar />
-<div class="pl-64 flex mt-1 ml-5 space-x-4">
-    <h1 class="font-bold items-center pt-5">My Forms</h1>
-    <div class="pt-3">
-        <Button innerText="Create Form" size="base" />
-    </div>
+<div class="pl-64 flex mt-1 ml-10 mr-10 justify-between pt-3">
+    <h1 class="font-bold items-center pt-2">My Forms</h1>
+    <Button innerText="Create Form" size="base" />
 </div>
-<div class="pl-64 bg-gray-100 h-screen space-x-4 mt-4">
-    <button
-        class="mt-4 p-5 ml-5 bg-white w-1/4 rounded-lg hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-gray-200 drop-shadow-lg"
-    >
-        <p>🥺</p>
-        <p>title</p>
-    </button>
-    <button
-        class="mt-4 p-5 ml-5 bg-white w-1/4 rounded-lg hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-gray-200 drop-shadow-lg"
-    >
-        <p>🥺</p>
-        <p>title</p>
-    </button>
+<div class="pl-64 bg-gray-100 h-screen space-x-4">
+    <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4 ml-4 mt-4 pt-4">
+        <button
+            class="p-10 bg-white col-span-1 rounded-lg hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-gray-200 drop-shadow-lg"
+        >
+            <p>🥺</p>
+            <p>title</p>
+        </button>
+        <button
+            class="p-10 bg-white col-span-1 rounded-lg hover:bg-gray-50 focus:ring-2 focus:outline-none focus:ring-gray-100 ring-offset-2 drop-shadow-lg"
+        >
+            <p>🥺</p>
+            <p>title</p>
+        </button>
+    </div>
 </div>
